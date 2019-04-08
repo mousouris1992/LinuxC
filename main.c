2cb844595a5d6bfb7e56110deb81b1373ca71e8b
Giwrgos <giorgosmousouris@gmail.com>
2019-04-08 14:04:29 +0300

    a

--- main.c
+++ main.c
@@ -1,9 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+typedef struct PRODUCT_ARGS
+{
+    int a;
+    int b;
+
+}PRODUCT_ARGS;
+
+void * mult(void * f_args)
+{
+    PRODUCT_ARGS * args = (PRODUCT_ARGS *)f_args;
+    int result = args->a * args->b;
+
+    printf("\n     ~Thread : (%i) * (%i) = %i" , args->a , args->b , result);
+    pthread_exit(NULL);
+}
 
 int main(int argc , char * argv[])
 {
 
-    printf("\nHello from Main3\n");
-    printf("\nReturning code : 0\n");
+    printf("\n-Hello from Main Thread\n");
+
+    PRODUCT_ARGS product_args;
+    product_args.a = 10;
+    product_args.b = 5;
+
+    pthread_t thread;
+    if ( pthread_create(&thread , NULL , mult , &product_args) != 0)
+    {
+        printf("\n-Error : Failed to create thread!");
+        exit(-1);
+    }
+
+    if ( pthread_join(thread , NULL) != 0)
+    {
+        printf("\n-Error : Failed to join() thread!");
+        exit(-1);
+    }
+
     return 0;
 }
