45079f8c8b132f0634425a294a755ddaf68ccd86
Giwrgos <giorgosmousouris@gmail.com>
2019-04-08 14:32:49 +0300

    a

--- main.c
+++ main.c
@@ -28,33 +28,9 @@ int main(int argc , char * argv[])
     printf("\n\n ----------- Main -----------\n");
 
 
-
-    //pthread_t thread1 , thread2;
     int count = 10;
-    pthread_t * threads = malloc(count * sizeof(pthread_t));
-
-    /*
-
-    PRODUCT_ARGS product_args;
-    product_args.a = 4;
-    product_args.b = 32;
-
-    if ( pthread_create(&threads[0] , NULL , mult , &product_args) != 0)
-    {
-        printf("\n-Error : Failed to create thread1!");
-        exit(-1);
-    }
-
-    PRODUCT_ARGS product_args2;
-    product_args2.a = 10;
-    product_args2.b = 5;
-
-    if ( pthread_create(&threads[1] , NULL , mult , &product_args2) !=0)
-    {
-        printf("\n-Error : Failed to create thread2!");
-        exit(-1);
-    }
-    */
+    //pthread_t * threads = malloc(count * sizeof(pthread_t));
+    pthread_t threads[count];
 
     srand(time(NULL));
     PRODUCT_ARGS product_args[count];
@@ -81,21 +57,8 @@ int main(int argc , char * argv[])
         }
     }
 
-    /*
-    if ( pthread_join(threads[0] , NULL) != 0)
-    {
-        printf("\n-Error : Failed to join() thread1!");
-        exit(-1);
-    }
 
-    if ( pthread_join(threads[1] , NULL) != 0)
-    {
-        printf("\n-Error : Failed to join() thread2!");
-        exit(-1);
-    }
-    */
-
-    free(threads);
+    //free(threads);
 
     printf("\n");
     return 0;
