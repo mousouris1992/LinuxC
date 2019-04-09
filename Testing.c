d290d853e84fd7f3dbba7720e3dfa3d3409f7949
giwrgos <giorgosmousouris@gmail.com>
2019-04-10 00:14:30 +0300

    a

--- Testing.c
+++ Testing.c
@@ -17,6 +17,8 @@ int getRandom(int min , int max)
     return (rand()%(max - min) + min);
 }
 
+#define BIL            1000000000L
+
 #define n_seat         250
 #define n_tel          8
 #define n_seatMin      1
@@ -32,8 +34,20 @@ int main()
 	srand(time(NULL)); // randomize seed
 
 
-	printf("\n random[%i , %i] : %i" ,n_seatMin , n_seatMax , getRandom(n_seatMin , n_seatMax));
-	printf("\n random[%i , %i] : %i" ,t_seatMin , t_seatMax , getRandom(t_seatMin , t_seatMax));
+	//printf("\n random[%i , %i] : %i" ,n_seatMin , n_seatMax , getRandom(n_seatMin , n_seatMax));
+	//printf("\n random[%i , %i] : %i" ,t_seatMin , t_seatMax , getRandom(t_seatMin , t_seatMax));
+
+	struct timespec
+	t_start,
+	t_end;
+
+	clock_gettime(CLOCK_REALTIME , &t_start);
+	sleep(2);
+	clock_gettime(CLOCK_REALTIME , &t_end);
+
+	double total_time = (t_end.tv_sec - t_start.tv_sec) + (t_end.tv_nsec - t_start.tv_nsec) / BIL;
+
+	printf("time : %d",total_time);
 
 	printf("\n");
 	return 0;
