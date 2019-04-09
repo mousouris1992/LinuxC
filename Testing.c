6025dc94487d7832d7a966136e695042a10cb62a
giwrgos <giorgosmousouris@gmail.com>
2019-04-09 21:11:20 +0300

    a

---
+++ Testing.c
@@ -0,0 +1,36 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//
+
+
+//-------------------------------------
+//
+//        Helper Functions
+//
+//-------------------------------------
+
+int getRandom(int min , int max)
+{
+    return (rand()%(max - min) + min);
+}
+
+#define n_seat         250
+#define n_tel          8
+#define n_seatMin      1
+#define n_seatMax      5
+#define t_seatMin      1
+#define t_seatMax      10
+#define p_cardSuccess  90
+#define c_seat         20
+
+
+int main()
+{
+	printf("\n random[%i , %i] : %i" ,n_seatMin , n_seatMax , getRandom(n_seatMin , n_seatMax));
+	printf("\n random[%i , %i] : %i" ,t_seatMin , t_seatMax , getRandom(t_seatMin , t_seatMax));
+
+	return 0;
+}
