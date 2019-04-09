8855af2104ad06066b4a09fb33527715b8ab3ff4
giwrgos <giorgosmousouris@gmail.com>
2019-04-09 20:42:45 +0300

    a

--- main.c
+++ main.c
@@ -4,10 +4,22 @@
 #include <time.h>
 
 //
+
 enum Operation
 {
-	_malloc,
-	something
+
+    mutex_lock,
+    mutex_unlock,
+    thread_cond_wait,
+    thread_cond_broadcast,
+    thread_cond_signal,
+    mutex_init,
+    thread_cond_init,
+    thread_create,
+    thread_join,
+    mutex_destroy,
+    thread_cond_destroy
+
 };
 
 //-------------------------------------
@@ -22,34 +34,61 @@ int getRandomFrom(int min , int max)
 }
 
 
-void checkStatus(void * data , enum Operation op , const char * obj , int return_type)
+void checkOperationStatus(enum Operation op ,const char * obj,  int rc , int return_type)
 {
-	char * op_name;
+    char * op_name;
+    switch(op)
+    {
+        case mutex_lock : op_name = "mutex_lock()";
+        break;
 
-	switch(op)
-	{
-		case _malloc :op_name = "malloc()";
-		break;
+        case mutex_unlock : op_name = "mutex_unlock()";
+        break;
 
-		case something :  op_name = "something()";
-		break;
-	}
+        case thread_cond_wait : op_name = "thread_cond_wait()";
+        break;
 
-	if(!data)
-	{
-		printf("\n-Error : %s::Failed to %s" , obj , op_name);
-		if(return_type == 0)
-		{
-			printf("\n--Terminating thread..");
-			pthread_exit(NULL);
-		}
-		else
-		{
-			printf("\n--Exiting program..");
-			exit(-1);
-		}
-	}
+        case thread_cond_broadcast :  op_name = "thread_cond_broadcast()";
+        break;
+
+        case thread_cond_signal : op_name = "thread_cond_signal()";
+        break;
+
+        case thread_create : op_name = "thread_create()";
+        break;
+
+        case thread_join :  op_name = "thread_join()";
+        break;
+
+        case mutex_init : op_name = "mutex_init()";
+        break;
+
+        case mutex_destroy : op_name = "mutex_destroy()";
+        break;
+
+        case thread_cond_init : op_name = "thread_cond_init()";
+        break;
+
+        case thread_cond_destroy : op_name = "thread_cond_destroy()";
+        break;
+
+    }
+
+    if(rc != 0)
+    {
+		printf("\n-Error : %s::Failed to %s | Return code : %i" , obj, op_name , rc);
+        if(return_type == 1)
+        {
+            exit(-1);
+        }
+        else
+        {
+            pthread_exit(&rc);
+        }
+    }
+
 }
+
 
 //-------------------------------------
 //
@@ -97,7 +136,7 @@ int random_seed = 0;
 //
 //-------------------------------------
 
-void * handleCustomer(void * customerId)
+void * handleCustomer(void * customer)
 {
 
 	return 0;
@@ -116,7 +155,6 @@ int main(int argc , char * argv[])
 
 
     /* ------------------- Initialize ------------------- */
-	// --------------------------------------------------
     if( argc != 3 )
     {
         printf("\n-Error : Program needs 2 arguments : { customers_count , seed_randomizer }");
@@ -127,9 +165,34 @@ int main(int argc , char * argv[])
 	customer_count = atoi(argv[1]);
 	random_seed    = atoi(argv[2]);
 
+	// Init customers
 	customers = malloc(customer_count * sizeof(Customer));
-	checkStatus(customers , _malloc , "customerThreads" , 1);
+	if(!customers)
+	{
+		printf("\n-Error : customers::Failed to malloc()");
+		printf("\n--Exiting program..");
+		exit(-1);
+	}
 
+	int rc;
+	for(int i = 0; i<customers_count; i++)
+	{
+		customers[i].Id = i;
+		rc = pthread_create(&customers[i].thread , NULL , handleCustomer , &customers[i] );
+		checkStatus(thread_create , "customers[]" , rc , 1);
+	}
+
+	/* -------------------------------------------------- */
+
+
+	/* ----------------- join threads ------------------- */
+	void * status;
+	for(int i = 0; i<customers_count; i++)
+	{
+		rc = pthread_join(customers[i].thread , &status);
+		checkOperationStatus(thread_join  , "customers[]" , rc , 1);
+	}
+	/* -------------------------------------------------- */
 
 
 	// clean up
