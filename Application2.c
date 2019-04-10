b0060775189e68446dced924c37f60b2577b6552
giwrgos <giorgosmousouris@gmail.com>
2019-04-10 22:57:14 +0300

    a

--- Application2.c
+++ Application2.c
@@ -352,7 +352,7 @@ int main(int argc , char * argv[])
 
 		for(int l = 0; l<zoneSize[i]; l++)
 		{
-			printf("LINE%i       " , l);
+			printf("  LINE%i  " , l);
 		}
 		printf("\n");
 		for(int seat = 0; seat<n_seat; seat++)
